0cd41eefdba26759d2d951b6c6ecfcf0143b2713
lemon-mint <ice.lemons.mint@gmail.com>
2022-02-17 20:42:27 +0900

    CheckOrigin: true

--- app.go
+++ app.go
@@ -52,6 +52,9 @@ func init() {
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func main() {
